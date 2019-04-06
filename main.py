07704c1be8b27ff4016a1fc2d50355c3f9c06761
jviriato <josevviriato@gmail.com>
2019-04-06 11:25:32 -0300

    toc

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from mc import MissionariesCannibals
 def main():
 	parser = ArgumentParser()
 	parser.add_argument("missionaries", help="number of missionaries", type=int)
-	parser.add_argument("cannibals", help="number of cannibals", type=int)
-  	parser.add_argument("boat_size", help="number of people who can be put on a boat", type=int)
+	parser.add_argument("cannibals", 	help="number of cannibals", type=int)
+  	parser.add_argument("boat_size", 	help="number of people who can be put on a boat", type=int)
 	args = parser.parse_args()
 
 	mc_problem = MissionariesCannibals(args.missionaries, args.cannibals, args.boat_size)
